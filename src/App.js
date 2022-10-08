c508c15101042f0aa73e72d585103e5773e35a01
NicoleJava <firstdayfish@gmail.com>
2022-10-08 23:11:14 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className='App'>
+      <div className='global-middle'>
+        Sakura
+      </div>
+      <div className='global-bottom'></div>
+    </div>
+  )
+}
+
+export default App;
